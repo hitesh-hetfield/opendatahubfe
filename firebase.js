3e9ec1a9b83fd3c920825e1466503ddb92de4bac
thedevpiyush <contactpiyushhere@gmail.com>
2025-02-19 14:37:33 +0530

    remove-firebase-file

--- firebase.js
+++ firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAaU9u3D9TzBqZXk-0EnKuxOoo3aFeeHC8",
   authDomain: "firechain-nft-marketplace.firebaseapp.com",
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: "1:678872325366:web:198e7857dfa8c9e96b60d7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
